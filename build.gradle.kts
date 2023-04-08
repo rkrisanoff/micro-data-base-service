eb3df264a088a3fe24af28ca69aa121e63880bf5
Крисанов Роман Валерьевич <284712@niuitmo.ru>
2023-04-08 11:46:18 +0300

    added annotationProcessor

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 
 group = "ifmo.dma"
-version = "0.0.1-SNAPSHOT"
+version = "0.9.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -30,11 +30,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	compileOnly("org.projectlombok:lombok")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
@@ -42,26 +40,15 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-	implementation("redis.clients:jedis:4.3.1")
 	implementation("com.fasterxml.jackson.core:jackson-databind")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-// https://mvnrepository.com/artifact/javax.persistence/javax.persistence-api
 	implementation("javax.persistence:javax.persistence-api:2.2")
-//
-//
-//	implementation("io.vertx:vertx-core")
-//	implementation("io.vertx:vertx-lang-kotlin")
-//
-//	// Vertx Web
-//	implementation("io.vertx:vertx-web")
-//
-//	// Vertx Rxjava
-//	implementation ("io.vertx:vertx-rx-java3")
-//	implementation ("io.vertx:vertx-rx-java3-gen")
-//
-//	// Vertx Redis
-//	implementation ("io.vertx:vertx-redis-client")
+
+
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+
 }
 allOpen {
 	annotation("jakarta.persistence.Entity")
