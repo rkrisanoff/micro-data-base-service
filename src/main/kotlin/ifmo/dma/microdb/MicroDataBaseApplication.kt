6ecdf179a20c639101ed611860d487a89d4580ad
Крисанов Роман Валерьевич <284712@niuitmo.ru>
2023-05-06 04:09:49 +0300

    remove trash

--- src/main/kotlin/ifmo/dma/microdb/MicroDataBaseApplication.kt
+++ src/main/kotlin/ifmo/dma/microdb/MicroDataBaseApplication.kt
@@ -1,19 +1,13 @@
 package ifmo.dma.microdb
 
-import ifmo.dma.microdb.services.CustomPersistenceExceptionTranslator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class MicroDataBaseApplication
 
-@Bean
-fun persistenceExceptionTranslator(): CustomPersistenceExceptionTranslator {
-    return CustomPersistenceExceptionTranslator()
-}
 fun main(args: Array<String>) {
     runApplication<MicroDataBaseApplication>(*args)
 }
